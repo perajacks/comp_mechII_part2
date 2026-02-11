993fd1fc91822d8f75d641298c4fb7cf46c36c56
perajacks <viper@june-23.com>
2026-02-11 21:13:35 +0200

    Delete comp_mechII_Project2-main directory

--- comp_mechII_Project2-main/post_processor.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class PostProcessor:
-    def __init__(self, mesh):
-        self.mesh = mesh
-
-    def export_displacements(self, U):
-        disp = U.reshape((-1, 3))
-        return disp
